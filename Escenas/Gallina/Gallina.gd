e34934987d348c3098f08148afd5253b915d9a21
Nacho <ignacioabadie@live.com.ar>
2018-02-13 13:24:09 -0300

    quitado codigo antiguo de gallina

--- Escenas/Gallina/Gallina.gd
+++ Escenas/Gallina/Gallina.gd
@@ -8,33 +8,21 @@ const JUMP_FORCE = -700
 
 var motionVector = Vector2()
 var direction = 1 # direction: 1 para derecha / -1 para izquierda
-var screenSize
 
 func _ready():
-	#Remover cuando se agreguen Botones para movimiento
 	$gallina_animacion.play()
-	screenSize = get_viewport_rect().size
-	pass
 
 func _physics_process(delta):
-	motionVector.y += GRAVITY
-	
-	var velocityVector = Vector2()
-	#Comportamiento de movimiento izquierda derecha automatico
-	if position.x + 48 >= screenSize.x:
-		direction = -1
-	if position.x - 48 <= 0:
-		direction = 1
+	motionVector.y += GRAVITY #set gravity vector
+
 	checkButtonPressed() # Check buttons to set direction and jump
 	checkAnimationDirection() # Set Animation direction
 	checkWallDirection() # Test if player was on wall
 	
 	motionVector.x = VELOCITY * direction
 	
-	move_and_slide(motionVector, NORMAL_VECTOR)
-	
 	# Set new position by direction result
-	#linear_velocity = Vector2(VELOCITY * direction, linear_velocity.y)
+	move_and_slide(motionVector, NORMAL_VECTOR)
 	
 func checkButtonPressed():
 	if is_on_floor():
