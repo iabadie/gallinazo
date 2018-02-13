4922aed6b7b147def7ae2df478dc62cadebf9c71
Nacho <ignacioabadie@live.com.ar>
2018-02-13 13:14:21 -0300

    Agregado de UI de usuario
    Se agregó la UI para manejar al usuario.
    Se modificó el comportamineto de la gallina acorde al manejo de fisicas de godot

--- Escenas/Gallina/Gallina.gd
+++ Escenas/Gallina/Gallina.gd
@@ -1,7 +1,12 @@
-extends Area2D
+extends KinematicBody2D
 
-const VELOCITY = 10
+const VELOCITY = 300
+const GRAVITY = 20
+const NORMAL_VECTOR = Vector2(0, -1)
+const JUMP_FORCE = -700
 
+
+var motionVector = Vector2()
 var direction = 1 # direction: 1 para derecha / -1 para izquierda
 var screenSize
 
@@ -11,18 +16,41 @@ func _ready():
 	screenSize = get_viewport_rect().size
 	pass
 
-func _process(delta):
+func _physics_process(delta):
+	motionVector.y += GRAVITY
+	
+	var velocityVector = Vector2()
 	#Comportamiento de movimiento izquierda derecha automatico
 	if position.x + 48 >= screenSize.x:
 		direction = -1
 	if position.x - 48 <= 0:
 		direction = 1
+	checkButtonPressed() # Check buttons to set direction and jump
+	checkAnimationDirection() # Set Animation direction
+	checkWallDirection() # Test if player was on wall
+	
+	motionVector.x = VELOCITY * direction
+	
+	move_and_slide(motionVector, NORMAL_VECTOR)
+	
+	# Set new position by direction result
+	#linear_velocity = Vector2(VELOCITY * direction, linear_velocity.y)
+	
+func checkButtonPressed():
+	if is_on_floor():
+		if Input.is_action_pressed("move_left"):
+			direction = -1
+		elif Input.is_action_pressed("move_right"):
+			direction = 1
+		if Input.is_action_just_pressed("jump"):
+			motionVector.y = JUMP_FORCE
 
-	# Set Animation direction
+func checkAnimationDirection():
 	if direction == 1:
 		$gallina_animacion.flip_h = false
 	else:
 		$gallina_animacion.flip_h = true
-	# Set new position by direction result
-	position.x = position.x + (VELOCITY * direction)
-	
+
+func checkWallDirection():
+	if is_on_wall():
+		direction = -direction	
