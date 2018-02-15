896c9fe31612654dea9230256b0a873c3f1681f3
Nacho <ignacioabadie@live.com.ar>
2018-02-15 00:41:03 -0300

    eliminar huevo al ser tocado

--- Escenas/Huevo/Huevo.gd
+++ Escenas/Huevo/Huevo.gd
@@ -1,7 +1,12 @@
-extends Node2D
+extends Area2D
 
 func _on_VisibilityEnabler2D_viewport_entered( viewport ):
 	$AnimacionHuevo.play("movimiento_huevo")
 
 func _on_VisibilityEnabler2D_viewport_exited( viewport ):
 	$AnimacionHuevo.stop()
+
+
+
+func _on_Huevo_area_entered( area ):
+	self.queue_free()
